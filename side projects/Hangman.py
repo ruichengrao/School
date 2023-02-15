ce47465baea9d127f1ca58aef04107292d90626c
Ruicheng Rao <rao.felix@hotmail.com>
2023-02-14 20:18:09 -0500

    added main function

--- side projects/Hangman.py
+++ side projects/Hangman.py
@@ -1,33 +1,24 @@
 import random
 GUESS = 6 
 wordlist = []
+gameover = False 
+letterList = []
 
-
-with open("words.txt") as file:
+with open("side projects\words.txt") as file:
 
     for each in file.readlines():
         splitWord = each.split()
         wordlist.append(splitWord)
 
-
 global word
 word = random.choice(wordlist)[0]
 print(word)
 
-letterList = [*word]
-print(letterList)
-guessLetter = input("Guess the letters:")
-
 def guessingLetters():
     for letter in word:
-        if letter not in guessLetter:
+        if letter not in letterList:
             return False
     return True
-
-if guessingLetters():
-    print("You Win!")
-else:
-    print("Not Done Yet!")
 
 printedWord = []
 
@@ -41,8 +32,31 @@ def printWord():
     global word
     letter_num = 0
     for letter in word:
-        if letter in guessLetter:
+        if letter in letterList:
             printedWord[letter_num] = letter
         else:
             printedWord[letter_num] = "_"
         letter_num += 1
+    print(",".join(printedWord))
+
+
+
+def main():
+    global guessingLetters
+    global word
+    global printedWord
+    global gameover
+    
+    while gameover == False:
+        letterGuess = input("Guess the letters:")
+        if letterGuess in letterList:
+            print("You have already guessed that letter!")
+        else:
+            guessingLetters()
+            printWord()
+            guesses = GUESS - 1
+        
+  
+
+
+main()
