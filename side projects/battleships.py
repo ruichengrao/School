1ea5c6c29f3bc75cda27ef195e27a4a129b51cdb
Ruicheng Rao <rao.felix@hotmail.com>
2023-03-28 20:31:35 -0400

    added hitting

--- side projects/battleships.py
+++ side projects/battleships.py
@@ -24,22 +24,39 @@ def printBoard(enemy_board):
        print(" ".join(row))
 
 def new_screen():
+    
     print("Battleships!")
+    print("My Board")
     printBoard(board)
+
+previous_inputs_x = set()
+previous_inputs_y = set()
 
 
 def main():
-    new_screen()
+    global x,y
+    while True:
+        new_screen()
+        print("___________________")
+        x = input("X-Coordinate:")
+        y = input("Y-Coorinate:")
+
+        if not x.isdigit() or not y.isdigit():
+            print("Inputs are not intergers!")
+        else: 
+            x = int(x)
+            y = int(y)
+            if y > 9 or x > 9 or y < 0 or x < 0:
+                print("Out of Bounds")
+            elif board[y][x] != '0':
+                print("This place has already been hit!")
+            else:
+                board[y][x] = 'X'
+                print("You Missed!")
+
 
 if __name__ == "__main__":
     main()
-
-
-print("My Board")
-
-print("-------------------")
-print("Enemy Board")
-printBoard(enemy_board)
 
 
     
