549201068eec031d439d2c1f0efe5f7d91547f96
Ruicheng Rao <rao.felix@hotmail.com>
2023-04-11 20:15:49 -0400

    added ship constructors

--- side projects/battleships.py
+++ side projects/battleships.py
@@ -1,3 +1,5 @@
+from random import random
+from random import randint
 board = []
 enemy_board = []
 
@@ -22,6 +24,41 @@ def printBoard(board):
 def printBoard(enemy_board):
     for row in enemy_board:
        print(" ".join(row))
+
+
+class Ships:
+    def __init__(self, name, size, health) -> None:
+        self.name = name
+        self.size = size
+        self.health = size
+        self.position = []
+        self.direction = int(random() * 2) * 90
+        empty_space = False
+        if self.direction == 0:
+            random_position(enemy_board, self.size, 0)
+        else:
+            random_position(enemy_board, 0, self.size)
+
+        while empty_space == False:
+            empty_space = True
+            if self.direction
+        
+
+def random_position(board, xBound, yBound):
+    xCoord = randint(0, len(board)- 1 - xBound)
+    yCoord = randint(0, len(board)- 1 - yBound)
+    return (xCoord,yCoord)
+
+ships = []
+ships.append(Ships("Submarine", 1, 1))
+ships.append(Ships("Corsair", 2, 2))
+ships.append(Ships("Cruiser", 3, 3))
+ships.append(Ships("Battleship", 4,4 ))
+ships.append(Ships("Carrier", 5,5))
+print(ships)
+
+
+
 
 def new_screen():
     
@@ -55,8 +92,11 @@ def main():
                 print("You Missed!")
 
 
+
+
 if __name__ == "__main__":
     main()
+    
 
 
     
